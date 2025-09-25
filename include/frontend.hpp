26fdf092b0f897e7947d176b7b0e64bd26af83b1
parker <rekrapt245@gmail.com>
2025-09-24 20:57:45 -0400

    added basic frontend api to compile

--- include/frontend.hpp
+++ include/frontend.hpp
@@ -4,3 +4,24 @@
 #include "Parser.hpp"
 #include "compiler.hpp"
 
+namespace sde::frontend{
+    
+template<class Num>
+Program<Num> compile_to_bytecode(const std::string& input, const Environment<Num>& env){
+    Lexer lexer(input);
+    std::vector<Token> tokens = lexer.lex();
+    Parser<Num> parser(env, std::move(tokens));
+    AST<Num> ast = parser.parse();
+    Compiler<Num> compiler;
+    return compiler.compile(ast);
+}
+
+template<class Num>
+AST<Num> parse_to_ast(const std::string& input, const Environment<Num>& env){
+    Lexer lexer(input);
+    std::vector<Token> tokens = lexer.lex();
+    Parser<Num> parser(env, std::move(tokens));
+    return parser.parse();
+}
+
+}
